2a5516ef8e7b9510f7cd6bab593d1ef31be3cae7
Alex Stolzoff <alexstolznut@hotmail.com>
2019-09-07 00:59:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   string = ""
-  colors.each {|x| string+= x[0,1].capitalize + ': ' + x + ', '}
-  puts string[0,-1] + '.'
+  colors.length.times-1 do |x|
+    string+= x[0,1].capitalize + ': ' + x + ', '
+  end
+  string+=colors[colors.length][0,1].capitalize + ': ' + colors[colors.length] +'.'
+  # puts "R: " + colors[0] + ", "+ "O: " + colors[1] + ", " + "Y: " + colors[2]+ ", " + "G: " + colors[3] +", " +
  end
